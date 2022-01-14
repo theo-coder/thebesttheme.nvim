9272fd6ea96edfb83e6babd72b5cec915294bd78
Théo Posty <theo@idfr.net>
2022-01-14 15:09:48 +0100

    fix error

--- lua/thebesttheme/util.lua
+++ lua/thebesttheme/util.lua
@@ -5,7 +5,7 @@ local util = {}
 function util.highlight(group, colors)
     local style = colors.style and "gui=" .. colors.style or "gui=NONE"
     local fg = colors.fg and "guifg=" .. colors.fg or "guifg=NONE"
-    local bg = colors.fg and "guibg=" .. colors.bg or "guibg=NONE"
+    local bg = colors.bg and "guibg=" .. colors.bg or "guibg=NONE"
     local sp = colors.sp and "guisp=" .. colors.sp or ""
 
     local hl = "highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp
